cc2b22081051c323b0e1c87bf35c994a755751bf
Dima Azarnykh <d.azarnykh@tum.de>
2019-06-14 14:41:24 +0200

    add pymysql

--- mlflow/store/dbmodels/db_types.py
+++ mlflow/store/dbmodels/db_types.py
@@ -4,12 +4,14 @@ Set of SQLAlchemy database schemas supported in MLflow for tracking server backe
 
 POSTGRES = 'postgresql'
 MYSQL = 'mysql'
+MYSQL2 = 'mysql+pymysql'
 SQLITE = 'sqlite'
 MSSQL = 'mssql'
 
 DATABASE_ENGINES = [
     POSTGRES,
     MYSQL,
+    MYSQL2,
     SQLITE,
     MSSQL
 ]
